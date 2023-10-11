4b21556e81c9a64337ab36caab6d61538f6f651e
love <yadavlove909@gmail.com>
2023-10-12 00:29:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      birthdate: '',
+      age: '',
+    };
+  }
+
+  calculateAge = () => {
+    const birthdate = new Date(this.state.birthdate);
+    const today = new Date();
+    const age = Math.floor((today - birthdate) / (365.25 * 24 * 60 * 60 * 1000));
+    this.setState({ age });
+  }
+
+  handleChange = (event) => {
+    this.setState({ birthdate: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Age Calculator</h1>
+        <div>
+          <label>Enter your birthdate:</label>
+          <input
+            type="date"
+            value={this.state.birthdate}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button onClick={this.calculateAge}>Calculate Age</button>
+        {this.state.age && <p>Your age is: {this.state.age} years old</p>}
+      </div>
+    );
+  }
 }
 
 export default App;
